6182a71d07290f72630a8940f35616c9b04474fe
codeninjascf <cnchandlersford@gmail.com>
2024-07-07 10:06:03 +0100

    Stack p2 Script Start

---
+++ sTACK/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Spawner spawner;
+    public CameraFollow view;
+    public UIManager uiManager;
+
+    private bool _gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
