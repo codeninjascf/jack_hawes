ea4a73f7e8cbc64a9d38cc3b8e9668c436670c8b
codeninjascf <cnchandlersford@gmail.com>
2024-07-28 10:06:10 +0100

    sketchhead playercontroller

--- SKETCH HEAD/Assets/Scripts/PlayerController.cs
+++ SKETCH HEAD/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,31 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10f;
     public float jumpForce = 15f;
 
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D _rigidbody;
 
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
+        float movement = moveSpeed * Input.GetAxis("Horizontal");
+
+        Player.Turn(_rigidbody, movement);
+
+        _rigidbody.position += movement * Time.deltaTime * Vector2.right;
         
+    }
+
+    void OnCollisionEnter2D()
+    {
+        if (_rigidbody.velocity.y <= 0)
+
+            
+        {
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
