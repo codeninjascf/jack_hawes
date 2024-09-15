a83f7258514244be3ef721f26efd4934423b16d2
codeninjascf <cnchandlersford@gmail.com>
2024-09-15 10:21:54 +0100

    y6ury

---
+++ MeanyBird-Jack-hawes/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Game manager object
+    [Header("Game Controller Object for controllingthe game")]
+    public GameController Controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
