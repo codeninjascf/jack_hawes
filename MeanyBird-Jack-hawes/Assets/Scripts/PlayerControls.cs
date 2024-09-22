e46583f9c85c6adc929cd784d43c192bc1db281d
codeninjascf <cnchandlersford@gmail.com>
2024-09-22 10:10:18 +0100

    coded the gamecontroller script

--- MeanyBird-Jack-hawes/Assets/Scripts/PlayerControls.cs
+++ MeanyBird-Jack-hawes/Assets/Scripts/PlayerControls.cs
@@ -7,10 +7,21 @@ public class PlayerControls : MonoBehaviour
     //Game manager object
     [Header("Game Controller Object for controllingthe game")]
     public GameController Controller;
+    [Header("Default Velocity")]
+    public float velocity = 1;
+    //Physics for the bird
+    private Rigidbody2D rb;
+    //height of the bird object on the y axis
+    private float objectHeight;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GameController = GetComponent<GameController>();
+        Time.timeScale = 1;
+        rb = GetComponent<Rigidbody2D>();
+        objectHeight
     }
 
     // Update is called once per frame
