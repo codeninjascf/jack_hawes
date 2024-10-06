eef0af9d861392aa8fe028b86a5998f3ae729bb5
codeninjascf <cnchandlersford@gmail.com>
2024-10-06 13:03:28 +0100

    Meany Bird on Spawner Script

--- MeanyBird-Jack-hawes/Assets/Scripts/PlayerControls.cs
+++ MeanyBird-Jack-hawes/Assets/Scripts/PlayerControls.cs
@@ -18,15 +18,16 @@ public class PlayerControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameController = GetComponent<GameController>();
+        Controller = GetComponent<GameController>();
         Time.timeScale = 1;
         rb = GetComponent<Rigidbody2D>();
-        objectHeight
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y/2;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+            if(Input.GetMouseButtonDown(0))
+            rb.velocity = Vector2.up * velocity;
     }
 }
--- MeanyBird-Jack-hawes/Assets/Scripts/Spawner.cs
+++ MeanyBird-Jack-hawes/Assets/Scripts/Spawner.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject Spikes;
+
+    public float height;
     // Start is called before the first frame update
     void Start()
     {
