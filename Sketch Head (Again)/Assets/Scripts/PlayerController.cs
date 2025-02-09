8f13c444e011b439c0674c82679f319162dd3c1f
codeninjascf <cnchandlersford@gmail.com>
2025-02-09 10:09:16 +0000

    platforms

--- Sketch Head (Again)/Assets/Scripts/PlayerController.cs
+++ Sketch Head (Again)/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 10f;
+    public float jumpForce = 15f;
+    private Rigidbody2D _rigedbody;
     void Start()
     {
-        
+        _rigedbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float movement = moveSpeed * Input.GetAxis("Horizontal");
+        Player.Turn(_rigedbody, movement);
+        _rigedbody.position += movement * Time.deltaTime * Vector2.right;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_rigedbody.velocity.y <= 0)
+            _rigedbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
