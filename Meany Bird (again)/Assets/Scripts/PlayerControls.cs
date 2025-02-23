6d7bcad6fe10eacffd15f2350ef6907e72c481d8
codeninjascf <cnchandlersford@gmail.com>
2025-02-23 10:35:32 +0000

    part 1 pg 8

---
+++ Meany Bird (again)/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Game Controller object for conctrolling the game")]
+    public GameController GameController;
+    [Header("Default Velocity")]
+    public float velocity = 1;
+    private Rigidbody2D rb;
+    private float objectHeight;
+
+    void Start()
+    {
+            gameController - GetComponent<CameController
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
