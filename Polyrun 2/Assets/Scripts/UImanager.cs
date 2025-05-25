4addf52ed13a0ef501b253c2f3a628dc5f39cab3
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-05-25 10:23:17 +0100

    code

--- Polyrun 2/Assets/Scripts/UImanager.cs
+++ Polyrun 2/Assets/Scripts/UImanager.cs
@@ -12,13 +12,18 @@ public class UImanager : MonoBehaviour
 
     public GameObject gameOverMenu;
     public TextMeshProUGUI gameOverScoreText;
-
+    private bool _GameOver;
+    private int _highScore;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoretext.gameObject.SetActive(false);
+        Mainmenu.SetActive(true);
+        gameOverMenu.SetActive(false);
+        _GameOver = false;
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     // Update is called once per frame
