3d9034bc70b97d898434d59e5d40c3826096ff19
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-08-03 10:10:52 +0100

    moving the clouds and their prefabs

---
+++ Cloud hop/Assets/VanishingPlatform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishingPlatform : MonoBehaviour
+{
+    private float vanishTime
+    private SpriteRenderer _SpriteRenderer;
+    private bool _vanishing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
