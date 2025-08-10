a03425b26ab7764947a92d82380f2e1e8bfd0176
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-08-10 10:20:31 +0100

    VanishingClouds

--- Cloud hop/Assets/VanishingPlatform.cs
+++ Cloud hop/Assets/VanishingPlatform.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class VanishingPlatform : MonoBehaviour
 {
-    private float vanishTime
+    private float vanishTime = 5f;
     private SpriteRenderer _SpriteRenderer;
     private bool _vanishing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    IEnumerator Vanish()
+    {
+       if (!_vanishing)
+        {
+            _vanishing = true;
+            while(_SpriteRenderer.color.a > 0)
+            {
+
+                _SpriteRenderer.color = new Color(_SpriteRenderer.color.r,_SpriteRenderer.color.g)
+                    _SpriteRenderer.color.b, _SpriteRenderer.color.a - 1 / 255f);
+                yield return new WaitForSeconds(vanishTime / 255f);
+            }
+            transform.DetachChildren();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (other.gameObject.CompareTag("player"))
+            startCorout
     }
 }
