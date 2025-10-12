b42b5205ddff58ee94bc809fbc33b8e6d6c5ea16
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-10-12 10:26:51 +0100

    Enemy controller

--- CyberFu/Assets/Scripts/EnemyController.cs
+++ CyberFu/Assets/Scripts/EnemyController.cs
@@ -16,8 +16,12 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GameManager.Player;
+        _animator = GetComponent<Animator>();
+        _Rigidbody = GetComponent<Rigidbody>();
     }
+     
+    
 
     // Update is called once per frame
     void Update()
