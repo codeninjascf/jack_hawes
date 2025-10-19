083ea6a4e080087773d3d521ff9165ffd829cb1c
codeninjascf <105656685+codeninjascf@users.noreply.github.com>
2025-10-19 10:08:41 +0100

    coding rotation and also variable packages

--- CyberFu/Assets/Scripts/EnemyController.cs
+++ CyberFu/Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -13,19 +14,52 @@ public class EnemyController : MonoBehaviour
     private Transform _player;
     private Animator _animator;
     private Rigidbody _Rigidbody;
+
+    private enum EnemyState
+    {
+        waiting,
+        Following,
+        Attacking
+    }
+    private EnemyState _state;
     // Start is called before the first frame update
     void Start()
     {
         _player = GameManager.Player;
         _animator = GetComponent<Animator>();
         _Rigidbody = GetComponent<Rigidbody>();
+        _state = EnemyState.Following;
     }
-     
-    
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.GameOver)
+        {
+            _state = EnemyState.waiting;
+        }
+        Vector3 direction = _player.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+        _animator.SetBool("walk", _state == EnemyState.Following);
+        _Rigidbody.isKinematic = _state != EnemyState.Following;
+
+        float distance = direction.magnitude;
+
+        switch (_state)
+        {
+            case EnemyState.Attacking when distance >= attackingDistance:
+                _state = EnemyState.waiting;
+                break;
+        }
+
+
+
+
+
+
+
     }
 }
